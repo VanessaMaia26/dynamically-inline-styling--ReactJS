0639175eb71ee58595257df70360f3942be85c2e
VanessaMaia26 <vanessamaia2607@gmail.com>
2022-09-09 15:15:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [padding, setPadding] = useState(0);
+
+  const handleClick = () => {
+    setPadding(20);
+  }
+
+  return (
+    <div>
+      <button
+      onClick={handleClick}
+      style={ backgroundColor: '#FF0000', color: '#FFF', border: '0'}
+      >Clique aqui</button>
+    </div>
+  )
+}
+
+export default App;
